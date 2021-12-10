02ac1f7816d4bafc9af80d5b0d43ed8634723fb4
sefi <seyfeddineghrimil8@gmail.com>
2021-12-10 23:51:24 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import "./components/css/main.css";
+import DisplayTodos from './components/DisplayTodos';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Todo App</h1> 
+      <Todos/>
+      <DisplayTodos/>
+
+    </div>
+  );
+}
+
+export default App;
